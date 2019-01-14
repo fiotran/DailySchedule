bef0be472d4accda38fa364b922b62963c870a76
Fiona <fiona.tran@ixup.com>
2019-01-14 18:28:41 +1100

    "with basic print"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { RouterModule } from '@angular/router';
+import { AddActivityComponent } from './pages/add-activity/add-activity.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ActivitiesService } from './pages/services/activities.service';
+import { CreatePlanComponent } from './pages/create-plan/create-plan.component';
+import { ViewPlanComponent } from './pages/view-plan/view-plan.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { PrintComponent } from './pages/components/print/print.component';
+import { APP_ROUTES } from './app.routing';
+import { ActivitiesComponent } from './pages/activities.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActivitiesComponent,
+    HeaderComponent,
+    AddActivityComponent,
+    CreatePlanComponent,
+    ViewPlanComponent,
+    PrintComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(APP_ROUTES),
+    BrowserAnimationsModule,
+    DragDropModule
   ],
-  providers: [],
+  providers: [ActivitiesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
