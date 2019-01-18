f45814abd7cc9476ae49f2315454223caa03057a
Gabriel George <gabriel.george92@gmail.com>
2019-01-18 11:04:54 +1000

    renamed pages to activities (as a feature)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
-import { ActivitiesService } from './pages/services/activities.service';
+import { ActivitiesService } from './activities/services/activities.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { APP_ROUTES } from './app.routing';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './core/header/header.component';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,6 +3,6 @@ import { Routes } from '@angular/router';
 export const APP_ROUTES: Routes = [
   {
     path: '',
-    loadChildren: './pages/activities.module#ActivitiesModule'
+    loadChildren: './activities/activities.module#ActivitiesModule'
   }
 ];
