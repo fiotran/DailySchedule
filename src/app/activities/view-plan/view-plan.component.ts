32920d6be6df75d18942e5b9deb344a21d9ce6e5
Fiona <fiona.tran@ixup.com>
2019-02-16 15:32:27 +1100

    '#s1.9'

--- src/app/activities/view-plan/view-plan.component.ts
+++ src/app/activities/view-plan/view-plan.component.ts
@@ -1,6 +1,6 @@
 import {
   Component, OnInit, Output,
-  EventEmitter,
+  EventEmitter, ViewChild, ElementRef
 } from '@angular/core';
 import { ActivityModel } from 'src/app/data/activity.model';
 import { ActivitiesService } from 'src/app/services/activities.service';
@@ -16,6 +16,7 @@ export class ViewPlanComponent implements OnInit {
   customisedValues: PlanModel;
   @Output() updateActivitiesList: EventEmitter<ActivityModel> = new EventEmitter<ActivityModel>();
   @Output() reloadActivities: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @ViewChild('dailySchedule') dailySchedule: ElementRef;
 
 
   constructor(private activitiesService: ActivitiesService) { }
@@ -40,6 +41,33 @@ export class ViewPlanComponent implements OnInit {
 
   drop(event: CdkDragDrop<any[]>) {
     moveItemInArray(this.planList, event.previousIndex, event.currentIndex);
+  }
+
+  print() {
+    let printContent, printWindow;
+    printContent = this.dailySchedule.nativeElement.innerHTML;
+    printWindow = window.open(
+      '',
+      '_blank',
+      'top=0,left=0,height=100%,width=auto'
+    );
+    printWindow.document.open();
+    printWindow.document.write(`
+      <html>
+        <head>
+          <title>Print tab</title>
+          <style>
+          //........Customized style.......
+          #content {  border: 5px solid red }
+          .screen-only {
+            display: none;
+            visibility: hidden;
+          }
+          </style>
+        </head>
+    <body onload="window.print();window.close()">${printContent}</body>
+      </html>`);
+    printWindow.document.close();
   }
 
 }
